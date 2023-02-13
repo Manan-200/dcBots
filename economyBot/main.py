74c5e3f48abcd0346afe78e6397566f76355bd09
Manan-200 <manandeopurkar@gmail.com>
2023-02-13 22:55:55 +0530

    Added rob and saveBreads functions

--- economyBot/main.py
+++ economyBot/main.py
@@ -4,6 +4,7 @@ import json
 import random
 
 DATA_FILE = "data.json"
+initialBreads = 10
 
 def loadData(filePath:str):
     try:
@@ -19,14 +20,18 @@ def saveData(filePath:str, data:dict):
 def loadBreads(guildID:discord.Interaction.guild_id, memberID:discord.Member.id, data:dict):
     if str(guildID) not in data:
         data[str(guildID)] = {}
-        data[str(guildID)][str(memberID)] = 0
+        data[str(guildID)][str(memberID)] = initialBreads
         saveData(DATA_FILE, data)
-        return 0
+        return initialBreads
     if str(memberID) not in data[str(guildID)]:
-        data[str(guildID)][str(memberID)] = 0
+        data[str(guildID)][str(memberID)] = initialBreads
         saveData(DATA_FILE, data)
-        return 0
+        return initialBreads
     return data[str(guildID)][str(memberID)]
+
+def saveBreads(guildID:discord.Interaction.guild_id, memberID:discord.Member.id, data:dict, breads:int):
+    data[str(guildID)][str(memberID)] = breads
+    saveData(DATA_FILE, data)
 
 TOKEN = loadData("token.json")["token"]
 
@@ -53,18 +58,40 @@ async def account_info(interaction:discord.Interaction, member:discord.Member=No
     breads = loadBreads(interaction.guild_id, member.id, data)
     await interaction.response.send_message(f"{member.name} has {breads} breads.")
 
-@bot.tree.command(name="guess_number", description="Guess the correct number(0/1) to win/lose breads!")
-async def guess_number(interaction:discord.Interaction, guess:int):
+@bot.tree.command(name="gamble", description="Guess the correct number(0/1) to win/lose breads!")
+async def gamble(interaction:discord.Interaction, guess:int):
     data = loadData(DATA_FILE)
     breads = loadBreads(interaction.guild_id, interaction.user.id, data)
     num = random.choice([0, 1])
     if num == guess:
-        breads += 5
-        await interaction.response.send_message("Correct! You won 5 breads. :D")
+        breads += 1
+        await interaction.response.send_message("Correct! You won a bread. :D")
     else:
-        breads -= 5
-        await interaction.response.send_message("Wrong, You lost 5 breads. D:")
-    data[str(interaction.guild.id)][str(interaction.user.id)] = breads
-    saveData(DATA_FILE, data)
+        if breads > 0:
+            breads -= 1
+            await interaction.response.send_message("Wrong, You lost a bread. D:")
+        else:
+            await interaction.response.send_message(f"Wrong, Try again. D:")
+    saveBreads(interaction.guild.id, interaction.user.id, data, breads)
 
+@bot.tree.command(name="rob", description="Rob a member!")
+async def rob(interaction:discord.Interaction, member:discord.Member):
+    if member.id != interaction.user.id:
+        data = loadData(DATA_FILE)
+        robberBreads = loadBreads(interaction.guild.id, interaction.user.id, data)
+        memberBreads = loadBreads(interaction.guild.id, member.id, data)
+        if memberBreads != 0:
+            breads = random.randrange(-5, 6)
+            robberBreads += breads
+            memberBreads -= breads
+            if breads >= 0:
+                await interaction.response.send_message(f"{interaction.user.name} robbed {breads} breads from {member.name}!")
+            else:
+                await interaction.response.send_message(f"{member.name} reverse robbed {breads*-1} breads from {interaction.user.name}!")
+            saveBreads(interaction.guild.id, interaction.user.id, data, robberBreads)
+            saveBreads(interaction.guild.id, member.id, data, memberBreads)
+        else:
+            await interaction.response.send_message(f"LMAO, {member} is broke.")
+    else:
+        await interaction.response.send_message("You can't rob yourself.")
 bot.run(TOKEN)
