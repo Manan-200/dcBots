9a6f27b408ce1ac366e7de894ed65d0a72b236a6
Manan-200 <manandeopurkar@gmail.com>
2023-02-18 18:51:21 +0530

    Added basic functioning of trackerBot

--- trackerBot/main.py
+++ trackerBot/main.py
@@ -1,20 +1,39 @@
+from discord.ext import commands, tasks
 from github import Github
 import json
+import discord
 
 with open("token.json", "r") as file:
-    gitToken = json.load(file)["git"]
+    data = json.load(file)
+    gitToken = data["git"]
+    botToken = data["bot"]
 
 g = Github(gitToken)
 repo = g.get_repo("Manan-200/dcBots")
 n = 0
+bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
 
 oldCommit = repo.get_commits()[0]
-while True:
-    if n == 10000:
-        n = 0
-        newCommit = repo.get_commits()[0]
-        if newCommit != oldCommit:
-            oldCommit = newCommit
-            print(f"Latest commit: {newCommit.commit.message}")
-            print(f"Author: {newCommit.commit.author.name}")
-    n += 1
+
+@tasks.loop(minutes=1)
+async def printCommit():
+    global oldCommit
+    for guild in bot.guilds:
+        channel = discord.utils.get(guild.channels, name="git")
+        if channel:
+            newCommit = repo.get_commits()[0]
+            if oldCommit != newCommit:
+                oldCommit = newCommit
+                await channel.send(f"New commit by {newCommit.commit.author.name} on {newCommit.repository.name}: '{newCommit.commit.message}'")
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user}")
+    try:
+        synced = await bot.tree.sync()
+        print(f"Synced {len(synced)} command(s)")
+        printCommit.start()
+    except Exception as e:
+        print(e)
+
+bot.run(botToken)
