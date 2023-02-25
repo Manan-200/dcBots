c665783be64e090a3da4f0e6551b8c3cac96a3d9
Manan-200 <manandeopurkar@gmail.com>
2023-02-25 13:35:00 +0530

    Uploaded to Replit, cleared data

--- trackerBot/main.py
+++ trackerBot/main.py
@@ -1,28 +1,36 @@
 from discord.ext import commands, tasks
 from github import Github
+from itertools import cycle
 import json
 import discord
 import requests
+import os
 
-def loadData(filePath:str):
+from keep_alive import keep_alive
+
+def loadData(filePath: str):
     try:
         with open(filePath, 'r') as file:
-            return(json.load(file))
+            return (json.load(file))
     except:
         return {}
-def saveData(filePath:str, data:dict):
+
+
+def saveData(filePath: str, data: dict):
     with open(filePath, 'w') as file:
         json.dump(data, file)
 
+
 DATA_FILE = "data.json"
 
-tokenData = loadData("tokens.json")
-gitToken = tokenData["git"]
-botToken = tokenData["bot"]
+gitToken = os.getenv("git")
+botToken = os.getenv("bot")
 headers = {"Authorization": gitToken}
 
 g = Github(gitToken)
 bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
+status = cycle(["tracking repos", "tracking..."])
+
 
 @bot.event
 async def on_ready():
@@ -34,8 +42,13 @@ async def on_ready():
     except Exception as e:
         print(e)
 
+
 @tasks.loop(minutes=1)
 async def printCommit():
+    #Keeping the bot alive
+    async def change_status():
+        await bot.change_presence(activity=discord.Game(next(status)))
+
     #Selecting channel to send messages
     for guild in bot.guilds:
         channel = discord.utils.get(guild.channels, name="git")
@@ -46,46 +59,57 @@ async def printCommit():
                 repo = g.get_repo(f"{path}")
                 oldNodeID = data[path]["nodeID"]
                 newNodeID = repo.get_commits()[0].sha
-                
+
                 #Comparing latest SHA with previous SHA
                 if newNodeID == oldNodeID:
                     continue
-                
+
                 #Saving new commit SHA and sending message on discord
                 fileUrl = f"https://github.com/{path}"
-                await channel.send(f"New commit by '{repo.get_commits()[0].commit.author.name}' on {fileUrl} : '{repo.get_commits()[0].commit.message}'")
+                await channel.send(
+                    f"New commit by '{repo.get_commits()[0].commit.author.name}' on {fileUrl} : '{repo.get_commits()[0].commit.message}'"
+                )
                 data[path]["nodeID"] = newNodeID
                 saveData(DATA_FILE, data)
 
-@bot.tree.command(name="track_file", description="Add a repository to tracking list")
-async def track_file(interaction:discord.Interaction, author:str, repo_name:str):
-    
+
+@bot.tree.command(name="track_file",
+                  description="Add a repository to tracking list")
+async def track_file(interaction: discord.Interaction, author: str,
+                     repo_name: str):
+
     path = f"{author}/{repo_name}"
 
     #Checking is entered repo path is valid
     url = f"https://api.github.com/repos/{path}"
     response = requests.get(url, headers=headers)
     if response.status_code != 200:
-        await interaction.response.send_message("The repository does not exist")
+        await interaction.response.send_message("The repository does not exist"
+                                                )
         return
-    
+
     data = loadData(DATA_FILE)
 
     fileUrl = f"https://github.com/{path}"
 
     #Checking if entered repo is already in data
     if path in data:
-        await interaction.response.send_message(f"{fileUrl} is already tracking list")
+        await interaction.response.send_message(
+            f"{fileUrl} is already tracking list")
         return
 
     #Saving the repo and latest commit SHA in data file
     repo = g.get_repo(path)
-    data[path] = {"nodeID":f"{repo.get_commits()[0].sha}"}
+    data[path] = {"nodeID": f"{repo.get_commits()[0].sha}"}
     saveData(DATA_FILE, data)
-    await interaction.response.send_message(f"Added {fileUrl} to tracking list")
+    await interaction.response.send_message(f"Added {fileUrl} to tracking list"
+                                            )
 
-@bot.tree.command(name="untrack_file", description="Remove a file from tracking list")
-async def untrack_file(interaction:discord.Interaction, author:str, repo_name:str):
+
+@bot.tree.command(name="untrack_file",
+                  description="Remove a file from tracking list")
+async def untrack_file(interaction: discord.Interaction, author: str,
+                       repo_name: str):
 
     data = loadData(DATA_FILE)
     path = f"{author}/{repo_name}"
@@ -95,21 +119,26 @@ async def untrack_file(interaction:discord.Interaction, author:str, repo_name:st
     try:
         del data[path]
         saveData(DATA_FILE, data)
-        await interaction.response.send_message(f"Removed {fileUrl} from tracking list")
+        await interaction.response.send_message(
+            f"Removed {fileUrl} from tracking list")
     #If repo doesn't exist, send error message
     except:
-        await interaction.response.send_message("The repository is not in the tracking list")
+        await interaction.response.send_message(
+            "The repository is not in the tracking list")
 
-@bot.tree.command(name="tracking_list", description="Prints list of repositories being tracked")
-async def tracking_list(interaction:discord.Interaction):
+
+@bot.tree.command(name="tracking_list",
+                  description="Prints list of repositories being tracked")
+async def tracking_list(interaction: discord.Interaction):
 
     data = loadData(DATA_FILE)
 
-    #Printing list of URLs of all repos in data 
+    #Printing list of URLs of all repos in data
     msgArr = []
     for path in data:
         fileUrl = f"https://github.com/{path}"
         msgArr.append(f"{fileUrl}")
     await interaction.response.send_message(f"{msgArr}")
 
+keep_alive()
 bot.run(botToken)
